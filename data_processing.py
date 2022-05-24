336d896d8d00f2cc139cfcb4ac1f49821a3ee7c1
yuki1003 <yuki1234@hotmail.nl>
2022-05-24 09:51:09 +0200

    include clear variable names in file

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,15 @@
+"""
+Authors:
+Group:
+Description: This file will only be used for data processing
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pyarrow.parquet as pq
 
-dataset = pd.read_parquet("yellow_tripdata_2022-03.parquet")
+pq_trips = pq.read_table("yellow_tripdata_2022-03.parquet")
+df_trips = trips.to_pandas()
+
+print(df_trips.head())
