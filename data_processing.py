1fe504981ffee4024a6fc92319b2e1958e4eb338
yuki1003 <yuki1234@hotmail.nl>
2022-05-24 09:54:03 +0200

    Fix variable names

--- data_processing.py
+++ data_processing.py
@@ -10,6 +10,8 @@ import numpy as np
 import pyarrow.parquet as pq
 
 pq_trips = pq.read_table("yellow_tripdata_2022-03.parquet")
-df_trips = trips.to_pandas()
+df_trips = pq_trips.to_pandas()
 
+# Exploring the data
 print(df_trips.head())
+print("Headers are: \n",df_trips.columns)
